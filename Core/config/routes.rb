9a86bf3f56dab431ec45f17051ea010e69b2d499
vagrant <vagrant@ansible-web>
2020-03-02 07:37:03 -0800

    add chart

--- Core/config/routes.rb
+++ Core/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :comixes
+  resources :comixes do
+    collection do
+      match '', via: :options, action: 'show'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
