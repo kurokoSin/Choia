3710d116c3ffae671d28a32534075ed1fac411a2
vagrant <vagrant@ansible-web>
2020-03-15 09:24:27 -0700

    Add Topics

--- Core/config/routes.rb
+++ Core/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :topics
   resources :comixes do
     collection do
       match '', via: :options, action: 'show'
--- Core/db/schema.rb
+++ Core/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_153122) do
+ActiveRecord::Schema.define(version: 2020_03_12_151855) do
 
   create_table "comixes", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_03_10_153122) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "week"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.string "week"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
