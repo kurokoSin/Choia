01b688508885d18c238b1d6c2d375fc018749886
vagrant <vagrant@ansible-web>
2020-07-20 07:27:58 -0700

    fix link

--- Crawler/register.rb
+++ Crawler/register.rb
@@ -10,8 +10,8 @@ require './Property.rb'
 # Core 接続先
 # Core ポート番号
 class Register
-  @destReg = "http://localhost:3000/comixes"
-  # @destReg = "http://localhost/comixes"
+  # @destReg = "http://localhost:3000/comixes"
+  @destReg = "http://localhost/comixes"
 
   def self.reg_book( bookinfo )
     
